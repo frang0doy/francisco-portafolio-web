fa13956a2bd3f979917349c2cc7d1e650990a392
Francisco Godoy <sonrigodoy65@gmail.com>
2025-02-23 22:15:10 -0300

    proyectos y servicios

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -1,4 +1,3 @@
-import AvatarServices from "@/components/avatar-services";
 import CircleImage from "@/components/circle-image";
 import SliderServices from "@/components/slider-services";
 import TransitionPage from "@/components/transition-page";
@@ -6,16 +5,20 @@ import TransitionPage from "@/components/transition-page";
 const ServicesPage = () => {
     return (
         <>
-
             <TransitionPage />
             <CircleImage />
-            <AvatarServices />
             <div className="grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2">
                 <div className="max-w-[450px]">
-
                     <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">Mis <span className="font-bold text-secondary"> servicios.</span></h1>
-                    <p className="mb-3 text-xl text-gray-300">Ofrezco servicios de desarrollo web frontend especializados en la creación de sitios web y aplicaciones atractivas y funcionales. Utilizando las últimas tecnologías, como HTML, CSS y JavaScript, diseño interfaces de usuario intuitivas y responsivas que reflejan la identidad de marca de mis clientes y mejoran su presencia en línea.</p>
-                    <button className="px-3 py-2 rounded-lg bg-secondary hover:bg-secondary/65">Contacta conmigo</button>
+                    <p className="mb-3 text-xl text-gray-300">Ofrezco servicios de desarrollo web, tanto en el frontend como en el backend. En el frontend, utilizo React, Vue.js, Angular, HTML, CSS, y JavaScript. En el backend, trabajo con PHP, Python, Next.js, y MySQL para crear sitios y aplicaciones web funcionales, atractivas y escalables que reflejan la identidad de marca de mis clientes.</p>
+                    <button 
+  className="px-3 py-2 rounded-lg bg-secondary hover:bg-secondary/65"
+>
+  <a href="mailto:franciscogodoy_@hotmail.com" className="text-white">
+    Contacta conmigo
+  </a>
+</button>
+
                 </div>
 
                 {/* SLIDER */}
